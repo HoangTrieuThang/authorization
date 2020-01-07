280ca67c069730e578f936e2234a1016e86ffda0
Your Name <valentine.tnhn>
2020-01-07 22:25:34 +0700

    push code

---
+++ oauth/src/main/java/com/example/oauth/Controller.java
@@ -0,0 +1,15 @@
+package com.example.oauth;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.security.Principal;
+
+@RestController
+public class Controller {
+
+    @GetMapping("/user/me")
+    public Principal user(Principal principal){
+        return principal;
+    }
+}
